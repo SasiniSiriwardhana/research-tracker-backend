5a37b34a154d30bdc185f7f2f6ea52289288534f
Sasini Siriwardhana <sasinisiriwardhana0106@gmail.com>
2026-07-13 00:45:41 +0530

    chore: initial project setup and dependencies

---
+++ src/main/java/lk/ijse/cmjd/research_tracker/ResearchTrackerApplication.java
@@ -0,0 +1,19 @@
+package lk.ijse.cmjd.research_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Research Project Tracker Application
+ * Entry point for the Spring Boot application.
+ *
+ * @author Research Tracker Team
+ * @version 1.0
+ */
+@SpringBootApplication
+public class ResearchTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/lk/ijse/cmjd/research_tracker/ResearchTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.cmjd.research_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+class ResearchTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
